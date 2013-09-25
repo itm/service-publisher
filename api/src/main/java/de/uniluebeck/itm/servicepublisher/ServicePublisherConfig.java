acabaad9e14c6a5ca8c83e3905e25b317c741c1e
Daniel Bimschas <daniel@bimschas.com>
2013-09-25 15:07:41 +0200

    Fixed bug in ServicePublisherConfig

--- api/src/main/java/de/uniluebeck/itm/servicepublisher/ServicePublisherConfig.java
+++ api/src/main/java/de/uniluebeck/itm/servicepublisher/ServicePublisherConfig.java
@@ -1,20 +1,23 @@
 package de.uniluebeck.itm.servicepublisher;
 
+import javax.annotation.Nullable;
+
 public class ServicePublisherConfig {
 
 	public static final int DEFAULT_PORT = 8080;
 
 	private int port = DEFAULT_PORT;
 
+	@Nullable
 	private String shiroIni;
 
 	@SuppressWarnings("unused")
 	public ServicePublisherConfig(final int port) {
-		this(port, "classpath:shiro.ini");
+		this(port, null);
 	}
 
 	@SuppressWarnings("unused")
-	public ServicePublisherConfig(final int port, final String shiroIni) {
+	public ServicePublisherConfig(final int port, @Nullable final String shiroIni) {
 		this.port = port;
 		this.shiroIni = shiroIni;
 	}
@@ -23,6 +26,7 @@ public class ServicePublisherConfig {
 		return port;
 	}
 
+	@Nullable
 	public String getShiroIni() {
 		return shiroIni;
 	}
